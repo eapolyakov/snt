23b3d82e3d25630bcc49b98f49907b529d1c5ef2
JP_Billy <jannykarper@gmail.com>
2020-10-23 22:29:43 +0300

    23_10_20/22_29

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -14,22 +14,26 @@ export default class Newsletter extends React.Component{
 
     addEmail(event){
         let formData = new FormData();
-        formData.append("text", this.state.value);
+        formData.append("email", this.state.value);
         fetch("http://edu.eapolyak.beget.tech/addEmail",{
             method: "POST",
             body: formData
-        });
+        }).then(response => {
+            response.json().then(
+               alert("Вы подписались на рассылку")
+            )
+        })
         event.preventDefault();
     }
 
-render(){
+    render(){
         return <div className="subscribe text-center">
             <div className="container">
                 <h3>Подпишитесь на рассылку новостей</h3>
                 <p>Что бы получать на свою электронную почту последние новости СНТ Металлист, решения собраний,
                     информацию о текущей хозяйственной деятельности, и другие необходимые сообщения</p>
                 <form onSubmit={this.addEmail}>
-                    <input type="email" placeholder="Email" className="user" value={this.state.value} onChange={this.handleChange} required />
+                    <input type="email" placeholder="Email" name="email" className="user" value={this.state.value} onChange={this.handleChange} required />
                     <input type="submit" value="Подписаться" />
                 </form>
                 <p className="spam">Вы будете получать только актуальную и полезную информацию</p>
